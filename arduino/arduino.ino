c98bd2c8143e42c925b2055e95c3d8efdcdc0511
Jegjag <Jegjag@DESKTOP-850F4TJ.home>
2018-04-18 01:09:06 +0100

    Arduino support finally added

---
+++ arduino/arduino.ino
@@ -0,0 +1,167 @@
+#define DATA_RATE 9600
+#define TIMEOUT 1000
+
+// Motor A pins
+#define A_BRAKE 9
+#define A_DIR 12
+#define A_ANALOG 3
+
+// Motor B pins
+#define B_BRAKE 8
+#define B_DIR 13
+#define B_ANALOG 11
+
+void setup()
+{
+    // Serial setup
+    Serial.begin(DATA_RATE);
+    Serial.setTimeout(TIMEOUT);
+    
+    pinMode(A_DIR, OUTPUT);  // Initialise Motor A
+    pinMode(B_DIR, OUTPUT);  // Initialise Motor B
+    
+    pinMode(A_BRAKE, OUTPUT);   // Initialise Motor A Brake
+    pinMode(B_BRAKE, OUTPUT);   // Initialise Motor B Brake
+}
+
+int readLine(char* buf, int bufferSize, String &ret)
+{
+    if(Serial.available() <= 0)
+    {
+        return -1;
+    }
+    
+    int n = Serial.readBytesUntil('\n', buf, bufferSize - 1);
+    if(n >= 0)
+    {
+        buf[n] = '\0';
+        ret = buf;
+    }
+    
+    return n;
+}
+
+String line;
+
+typedef enum
+{
+    A, B
+} Motor;
+
+typedef enum
+{
+    FORWARD, BACKWARD
+} Direction;
+
+inline void brake(Motor motor)
+{
+    // Brake
+    if(motor == A)
+    {
+        digitalWrite(A_BRAKE, HIGH);
+    }
+    else if(motor == B)
+    {
+        digitalWrite(B_BRAKE, HIGH);
+    }
+}
+
+inline void setMotor(Motor m, Direction d, int spd)
+{
+    if(m == A)
+    {
+        digitalWrite(A_BRAKE, LOW);
+        
+        if(d == FORWARD)
+        {
+            digitalWrite(A_DIR, HIGH);
+        }
+        else if(d == BACKWARD)
+        {
+            digitalWrite(A_DIR, LOW);
+        }
+        
+        analogWrite(A_ANALOG, spd);
+    }
+    else if(m == B)
+    {
+        digitalWrite(B_BRAKE, LOW);
+        
+        if(d == FORWARD)
+        {
+            digitalWrite(B_DIR, HIGH);
+        }
+        else if(d == BACKWARD)
+        {
+            digitalWrite(B_DIR, LOW);
+        }
+        
+        analogWrite(B_ANALOG, spd);
+    }
+}
+
+void loop()
+{
+    int n = readLine(buffer, sizeof(buffer), line);
+    
+    if(n <= 0)
+    {
+        return;
+    }
+    
+    // Process line
+    // Motor
+    Motor motor;
+    String line_noMotor;
+    if(line.startsWith("A"))
+    {
+        motor = A;
+        line_noMotor = line.replace("A", "");
+    }
+    else if(line.startsWith("B"))
+    {
+        motor = B;
+        line_noMotor = line.replace("B", "");
+    }
+    else
+    {
+        return;
+    }
+    
+    // Direction (Up/Down to not conflict B for backwards)
+    Direction d;
+    String line_noDir;
+    if(line.startsWith("U"))
+    {
+        d = FORWARD;
+        line_noDir = line.replace("U", "");
+    }
+    else if(line.startsWith("D"))
+    {
+        d = BACKWARD;
+        line_noDir = line.replace("D", "");
+    }
+    else if(line.startsWith("S"))
+    {
+        brake(motor);
+        return;
+    }
+    else
+    {
+        return;
+    }
+    
+    // Speed
+    int spd = line_noDir.toInt();
+    if(spd <= 0)
+    {
+        brake(motor);
+        return;
+    }
+    if(spd > 255)
+    {
+        spd = 255;
+    }
+    
+    setMotor(motor, d, spd);
+}
---
+++ src/robot/core/Arduino.java
@@ -0,0 +1,162 @@
+package robot.core;
+
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Enumeration;
+
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+
+/**
+ * Taken from: https://gist.github.com/tkojitu/1924274
+ */
+public class Arduino implements Closeable
+{
+	private static final String[] PORT_NAMES = new String[]
+	{
+		// Linux
+		"/dev/ttyUSB0",
+		
+		// Windows
+		"COM3",
+		
+		// Mac
+		"/dev/tty.usbserial-A9007UX1"
+	};
+	
+	private static final int TIMEOUT = 2000;	// ms
+	private static final int DATA_RATE = 9600;
+	
+	private SerialPort serialPort;
+	private OutputStream output;
+	private ByteArrayOutputStream baos;
+	private OutputStreamWriter writer;
+	
+	public Arduino()
+	{
+		baos = new ByteArrayOutputStream();
+		writer = new OutputStreamWriter(baos);
+		
+		CommPortIdentifier id = findPortID();
+		if(id == null)
+		{
+			System.err.println("Could not find COM port.");
+			System.exit(-1);
+		}
+		
+		try
+		{
+			serialPort = (SerialPort) id.open(this.getClass().getName(), TIMEOUT);
+			serialPort.setSerialPortParams
+			(
+				DATA_RATE,
+				SerialPort.DATABITS_8,
+				SerialPort.STOPBITS_1,
+				SerialPort.PARITY_NONE
+			);
+			output = serialPort.getOutputStream();
+			serialPort.notifyOnDataAvailable(true);
+		}
+		catch(Exception e)
+		{
+			System.err.println("Failed to bind to port.\n\nStacktrace:");
+			e.printStackTrace();
+			
+			System.exit(-1);
+		}
+	}
+	
+	public static enum Motor
+	{
+		A("A"),
+		B("B");
+		
+		final String symbol;
+		
+		Motor(String symbol)
+		{
+			this.symbol = symbol;
+		}
+	}
+	
+	public static enum Direction
+	{
+		FORWARD("U"),
+		BACKWARD("D"),
+		BRAKE("S");
+		
+		final String symbol;
+		
+		Direction(String symbol)
+		{
+			this.symbol = symbol;
+		}
+	}
+	
+	/**
+	 * @param m - the motor to apply to
+	 * @param d - the direction
+	 * @param speed - must be between 1 and 255 (0 means brake)
+	 */
+	public void send(Motor m, Direction d, int speed)
+	{
+		sendString(m.symbol + d.symbol + speed);
+	}
+	
+	/**
+	 * <strong>Format: MDS</strong><br>
+	 * M - Motor<br>
+	 * D - Direction<br>
+	 * S - Speed<br>
+	 * <br>
+	 * <i>Example: <code>AF255</code></i>
+	 */
+	public void sendString(String command)
+	{
+		try
+		{
+			writer.write(command, 0, command.length());
+			writer.flush();
+			
+			baos.writeTo(output);
+			baos.reset();
+		}
+		catch(IOException e)
+		{
+			System.err.println("Failed to send command \"" + command + "\".\n\nStacktrace:");
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public synchronized void close()
+	{
+		if(serialPort == null)	return;
+		
+		serialPort.close();
+	}
+	
+	private final CommPortIdentifier findPortID()
+	{
+		CommPortIdentifier id = null;
+		Enumeration<?> portEnum = CommPortIdentifier.getPortIdentifiers();
+		
+		while(portEnum.hasMoreElements())
+		{
+			CommPortIdentifier currentPortID = (CommPortIdentifier) portEnum.nextElement();
+			for(String portName : PORT_NAMES)
+			{
+				if(currentPortID.getName().equals(portName))
+				{
+					id = currentPortID;
+					break;
+				}
+			}
+		}
+		
+		return id;
+	}
+}
