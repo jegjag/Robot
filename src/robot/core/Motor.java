b6a2ce04ba3750d7ab4b343d8987376657e52f8f
Jegjag <jegjagg@gmail.com>
2018-04-27 00:50:17 +0100

    Split class

---
+++ src/robot/core/Motor.java
@@ -0,0 +1,34 @@
+package robot.core;
+
+public class Motor
+{
+	public Direction direction;
+	public int speed;
+	
+	public static enum MotorID
+	{
+		A("A"),
+		B("B");
+		
+		final String symbol;
+		
+		MotorID(String symbol)
+		{
+			this.symbol = symbol;
+		}
+	}
+	
+	public static enum Direction
+	{
+		FORWARD("U"),
+		BACKWARD("D"),
+		BRAKE("S");
+		
+		final String symbol;
+		
+		Direction(String symbol)
+		{
+			this.symbol = symbol;
+		}
+	}
+}
